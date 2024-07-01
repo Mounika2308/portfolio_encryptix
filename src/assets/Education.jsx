ac1309983f4b00c464acec41aedd3ec05fa4651c
Mounika2308 <mounikasiripuram71@gmail.com>
2024-07-01 21:23:16 +0530

    first commit

---
+++ src/assets/Education.jsx
@@ -0,0 +1,32 @@
+function Education(){
+    return (
+        <section  id="education">
+        <h3>Education</h3>
+        <div className="inner">
+            <div className="logo"><img src="/iitg_logo.png"></img></div>
+            <div className="description">
+                <h6>Bsc Biotechnology in SR University </h6>
+                <p>Mar. 2018 - 2021 </p>
+                <br></br>
+                <p>Position of Responsibility :</p>
+                <div>
+                    <ul>
+                        <li>Placement Volunteering , <a href="https://iitg.ac.in/ccd/">Placement cell IIT Guwahati</a></li>
+                        <li>Web Developer  <a href="https://iitg.ac.in/sa/tedxiitg/index.html">TEDxIITGuwahati</a> </li>
+                        <li>Workshops Executive, <a href="https://techniche.org/">Techniche, IIT Guwahati</a></li>
+                    </ul>                
+                </div>
+                <p>Relevent Coursework :</p>
+                <div>
+                    <ul>
+                        <li>Biotechnology:about technology and pcr techiniques</li>
+                        <li>Biochemistry:about chemical engineering</li>
+                        <li>Biology : Bioinformatics, Biological data analysis</li>
+                    </ul>                
+                </div>
+            </div>
+        </div>
+      </section>
+    )
+}
+export default Education;
---
+++ src/assets/Header.jsx
@@ -0,0 +1,16 @@
+function Header(){
+    return (
+        <header>
+            <h1>Mounika Siripuram</h1>
+            <p>Personal Email :<a href="mailto:skrsumit250@gmail.com"> mounikasiripuram71@gmail.com</a></p>
+            <nav>
+                <a href="#education">Education</a>
+                |<a href="#skills">Skills</a>
+                |<a href="#experience">Experience</a>
+                |<a href="#projects">Projects</a>
+                |<a href="#contact">Contact</a>
+            </nav>
+        </header>
+    )
+}
+export default Header;
